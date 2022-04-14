8fc4351897eae015d6996bf83b1fab7032995c75
Robert <bigbob1991@gmail.com>
2022-04-14 17:38:36 +0400

    dependencies updated

--- camerax-demo/build.gradle.kts
+++ camerax-demo/build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-        classpath("com.android.tools.build:gradle:7.1.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
     }
 }
 
