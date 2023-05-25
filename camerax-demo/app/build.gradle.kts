f2d20af04c7d3ce55c7807000a37f709d6b390ca
Robert <bigbob1991@gmail.com>
2023-05-25 14:15:57 +0400

    camerax updated

--- camerax-demo/app/build.gradle.kts
+++ camerax-demo/app/build.gradle.kts
@@ -22,38 +22,39 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
     }
+    namespace = "com.robertlevonyan.demo.camerax"
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("com.google.android.material:material:1.9.0-alpha01")
+    implementation("com.google.android.material:material:1.9.0-rc01")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.camera:camera-core:1.3.0-alpha03")
-    implementation("androidx.camera:camera-camera2:1.3.0-alpha03")
-    implementation("androidx.camera:camera-lifecycle:1.3.0-alpha03")
-    implementation("androidx.camera:camera-extensions:1.3.0-alpha03")
-    implementation("androidx.camera:camera-view:1.3.0-alpha03")
-    implementation("androidx.fragment:fragment:1.5.5")
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.camera:camera-core:1.3.0-alpha07")
+    implementation("androidx.camera:camera-camera2:1.3.0-alpha07")
+    implementation("androidx.camera:camera-lifecycle:1.3.0-alpha07")
+    implementation("androidx.camera:camera-extensions:1.3.0-alpha07")
+    implementation("androidx.camera:camera-view:1.3.0-alpha07")
+    implementation("androidx.fragment:fragment:1.5.7")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
 
-    implementation("io.coil-kt:coil:2.2.2")
-    implementation("io.coil-kt:coil-video:2.2.2")
+    implementation("io.coil-kt:coil:2.3.0")
+    implementation("io.coil-kt:coil-video:2.3.0")
 }
--- camerax-demo/build.gradle.kts
+++ camerax-demo/build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
-        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+        classpath("com.android.tools.build:gradle:8.0.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
     }
 }
