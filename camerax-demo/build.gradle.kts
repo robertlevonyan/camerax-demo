b265b523c719c237d93b5b09f56f24b5d516947e
Robert Levonyan <bigbob1991@gmail.com>
2025-04-17 10:54:06 +0400

    bump dependencies

--- camerax-demo/build.gradle.kts
+++ camerax-demo/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
-        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.android.tools.build:gradle:8.9.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.4")
     }
 }
